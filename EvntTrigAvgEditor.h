7e9342f2e909019bfeb45635f2399f316748298d
Claybarn <barnes.clayton@icloud.com>
2017-09-09 18:41:53 -0400

    Update EvntTrigAvgEditor.h

--- EvntTrigAvgEditor.h
+++ EvntTrigAvgEditor.h
@@ -21,215 +21,61 @@
 
 */
 
-#ifndef EvntTrigAvgCANVAS_H_
-#define EvntTrigAvgCANVAS_H_
+#ifndef __EvntTrigAvgEDITOR_H_F0BD2DD9__
+#define __EvntTrigAvgEDITOR_H_F0BD2DD9__
 
-#include <VisualizerWindowHeaders.h>
-#include "EvntTrigAvg.h"
+#include <VisualizerEditorHeaders.h>
 
-#include <vector>
+
+class EvntTrigAvgCanvas;
+class EvntTrigAvg;
 
 /**
-
  
-*/
+User interface for EvntTrigAvg
 
-class EvntTrigAvgDisplay;
-class Timescale;
-class GraphUnit;
-class LabelDisplay;
-class HistoGraph;
-class StatDisplay;
+@see EvntTrigAvg, EvntTrigAvgCanvas
+ */
 
-
-class EvntTrigAvgCanvas : public Visualizer, public Button::Listener
-
+class EvntTrigAvgEditor : public VisualizerEditor,
+    public Label::Listener,
+    public ComboBox::Listener
 {
+    
 public:
-    EvntTrigAvgCanvas(EvntTrigAvg* n);
-    ~EvntTrigAvgCanvas();
-
-    void paint(Graphics& g);
-
-    void refresh();
+    
+    EvntTrigAvgEditor(GenericProcessor* parentNode, bool useDefaultParameterEditors);
+    virtual ~EvntTrigAvgEditor();
+    void buttonEvent(Button* button);
+    void labelTextChanged(Label* label);
+    void comboBoxChanged(ComboBox* comboBox);
+    void sliderEvent(Slider* slider);
+    void channelChanged (int chan, bool newState) override;
+    void updateSettings();
+    Visualizer* createNewCanvas();
+    
+    EvntTrigAvgCanvas* evntTrigAvgCanvas;
 
     
-    void beginAnimation();
-    void endAnimation();
-
-    void refreshState();
-
-    void setParameter(int, float) {}
-    void setParameter(int, int, int, float) {}
-
-    void update();
-
-    void resized();
-
-    bool keyPressed(const KeyPress& key);
-
-    void buttonClicked(Button* button);
-
-    void startRecording() { }; // unused
-    void stopRecording() { }; // unused
-    void setBin(int bin_);
-    void setBinSize(int binSize_);
-    void setData(int data_);
+private:
+    struct EventSources
+    {
+        unsigned int eventIndex;
+        unsigned int channel;
+    };
+    std::vector<EventSources> eventSourceArray;
+    
+    
     EvntTrigAvg* processor;
+    ScopedPointer<ComboBox> triggerChannel;
+    ScopedPointer<Label> binSize, windowSize, channelLabel, binLabel, windowLabel;
+    Font font;
 
-   
-   // ScopedPointer<UtilityButton>
-
-private:
-    
-    std::vector<std::vector<uint64*>> histoData;
-    std::vector<std::vector<float>> minMaxMean;
-    void removeUnitOrBox();
-    ScopedPointer<Viewport> viewport;
-    ScopedPointer<EvntTrigAvgDisplay> display;
-    ScopedPointer<UtilityButton> clearHisto;
-    //  SpikeObject spike;
-    int scrollBarThickness;
-    int border = 20;
-    int triggerChannel = -1;
-    unsigned long spikeBufferSize = 0;
-    unsigned long ttlBufferSize = 0;
-    int bin = 0;
-    int binSize = 0;
-    int data = 0;
-    Timescale* scale;
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(EvntTrigAvgCanvas);
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(EvntTrigAvgEditor);
 
 };
-///////////////////////////////////////////////////////////////////////////////////
-
-class EvntTrigAvgDisplay : public Component
-{
-    
-public:
-    EvntTrigAvgDisplay(EvntTrigAvgCanvas* c, Viewport* v, EvntTrigAvg* p);
-    ~EvntTrigAvgDisplay();
-    void visibleAreaChanged (const Rectangle<int>& newVisibleArea);
-    
-    /** Callback method that is called when the viewed component is added, removed or swapped. */
-    void viewedComponentChanged (Component* newComponent);
-    void resized();
-    void paint(Graphics &g);
-    void refresh();
-    int getNumGraphs();
-private:
-    CriticalSection mut;
-    EvntTrigAvg* processor;
-    EvntTrigAvgCanvas* canvas;
-    Viewport* viewport;
-    std::vector<GraphUnit*> graphs;
-    juce::Colour channelColours[16];
-    std::vector<std::vector<uint64*>> histoData;
-    std::vector<std::vector<float*>> minMaxMean;
-    int border = 20;
-    
-};
-
-//---------------------------
-
-class Timescale : public Component
-{
-public:
-    Timescale(int windowSize_, uint64 sampleRate_, int data = 0, int bin = 0 ,int binSize = 0 );
-    ~Timescale();
-    void paint(Graphics& g);
-    void resized();
-    void update(int windowSize, uint64 sR);
-    void setBin(int bin_);
-    void setData(int data_);
-    void setBinSize(int binSize_);
-private:
-    int windowSize;
-    uint64 sampleRate;
-    int bin = 0;
-    int binSize = 0;
-    int data = 0;
-    int lowerBin = 0;
-    int upperBin = 0;
-    
-};
 
 
-//---------------------------
 
 
-class GraphUnit : public Component
-{
-public:
-    GraphUnit(EvntTrigAvg* processor_,EvntTrigAvgCanvas* canvas_,juce::Colour color_, String name_, float* stats_, uint64*data_);
-    ~GraphUnit();
-    void paint(Graphics& g);
-    void resized();
-private:
-    LabelDisplay* LD;
-    HistoGraph* HG;
-    StatDisplay* SD;
-    Colour color;
-};
-
-//---------------------------
-
-class LabelDisplay : public Component
-{
-public:
-    LabelDisplay(Colour color,String n);
-    ~LabelDisplay();
-    void paint(Graphics& g);
-    void resized();
-private:
-    Colour color;
-    String name;
-};
-
-//---------------------------
-
-class HistoGraph : public Component
-{
-    
-public:
-    HistoGraph(EvntTrigAvgCanvas* canvas_,juce::Colour color_, int bins_, int max_, uint64* histoData_);
-    ~HistoGraph();
-    
-    void paint(Graphics& g);
-    void resized();
-    
-    void select();
-    void deselect();
-    void clear();
-    
-    void mouseMove(const MouseEvent &event);
-    
-    //void buttonClicked(Button* button);
-    
-private:
-    int bins = 0;
-    Colour color;
-    int max;
-    uint64* histoData;
-    int valueY=0;
-    EvntTrigAvgCanvas* canvas;
-    
-};
-
-//---------------------------
-
-class StatDisplay : public Component
-{
-public:
-    StatDisplay(juce::Colour c, float* s);
-    ~StatDisplay();
-    void paint(Graphics& g);
-    void resized();
-private:
-    Colour color;
-    float* stats;
-    
-};
-
-
-#endif  // EvntTrigAvgCANVAS_H_
+#endif  // __EvntTrigAvgEDITOR_H_F0BD2DD9__
